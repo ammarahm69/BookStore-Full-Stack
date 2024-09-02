39d60b3a38021a66eaa5cc11803a39f632d355a9
Ammar Ahmed <ammar69aa59@gmail.com>
2024-09-01 20:39:39 -0700

    react-router-installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
+import Home from './pages/Home'
 import './App.css'
-
 function App() {
   return (
     <>
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home />
     </>
   )
 }
 
-export default App
+export default App;
